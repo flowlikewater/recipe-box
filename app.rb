81ee4c076d79932bd7bd0ad97b09ca0b0aeb8296
Kevin <cho.kevin@hotmail.com>
2017-04-12 17:11:57 +0800

    added ingredient and recipe relationship, delete button, add ingredients

--- app.rb
+++ app.rb
@@ -9,17 +9,17 @@ get ("/") do
   erb(:index)
 end
 
-post ('/tags') do
-  name=params.fetch("name")
-  Tag.create(:name => name)
-  redirect('/')
-end
+  post ('/tags') do
+    name=params.fetch("name")
+    Tag.create(:name => name)
+    redirect("/")
+  end
 
-delete ('/tags') do
+  delete ('/tags') do
     tag=Tag.find(params.fetch('id').to_i)
     tag.delete
     redirect("/")
-end
+  end
 
 get ('/tags/:id') do
   @tag=Tag.find(params.fetch('id').to_i)
@@ -27,11 +27,43 @@ get ('/tags/:id') do
   erb(:tag)
 end
 
-post ('/recipes') do
-  name=params.fetch("name")
-  self_id=params.fetch('self_id')
-  tag=Tag.find(params.fetch('self_id'))
-  recipe = Recipe.create(:name => name)
-  recipe.tags << tag     #<< means array appending character , ActiveRecord use this to update relation join table
-  redirect("/tags/#{self_id}")
+  post ('/recipes') do
+    name=params.fetch("name")
+    self_id=params.fetch('self_id')
+    tag=Tag.find(params.fetch('self_id'))
+    recipe = Recipe.create(:name => name)
+    recipe.tags << tag     #<< means array appending character , ActiveRecord use this to update relation join table
+    redirect("/tags/#{self_id}")
+  end
+
+  delete ('/recipes') do
+    recipe=Recipe.find(params.fetch('id').to_i)
+    recipe.delete
+    self_id=params.fetch('self_id')
+    redirect("/tags/#{self_id}")
+  end
+
+get ('/tags/:id/recipes/:id2') do
+  @tag=Tag.find(params.fetch('id').to_i)
+  @recipe=Recipe.find(params.fetch('id2').to_i)
+  @ingredients=@recipe.ingredients
+  erb(:ingredients)
 end
+
+  post ('/ingredients') do
+    tag_id = params.fetch('tag_id').to_i
+    recipe_id = params.fetch('recipe_id').to_i
+    name=params.fetch("name")
+    ingredient = Ingredient.create(:name => name)
+    recipe = Recipe.find(recipe_id)
+    ingredient.recipes << recipe     #<< means array appending character , ActiveRecord use this to update relation join table
+    redirect("/tags/#{tag_id}/recipes/#{recipe_id}")
+  end
+
+  delete ('/ingredients') do
+    tag_id = params.fetch('tag_id').to_i
+    recipe_id = params.fetch('recipe_id').to_i
+    ingredient= Ingredient.find(params.fetch('id'))
+    ingredient.delete
+    redirect("/tags/#{tag_id}/recipes/#{recipe_id}")
+  end
